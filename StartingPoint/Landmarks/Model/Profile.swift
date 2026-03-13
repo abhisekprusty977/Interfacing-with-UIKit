cb8aacf49f910a677b927fe840d2285fbecc61e2
abhisekprusty977 <abhisekprusty977@gmail.com>
2026-03-14 01:29:36 +0530

    Implement Interfacing-with-UIKit

---
+++ StartingPoint/Landmarks/Model/Profile.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+
+    static let `default` = Profile(username: "abhisekprusty977")
+
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+
+        var id: String { rawValue }
+    }
+}
